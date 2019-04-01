2dd35824250599447d27ba6836a6c5b90373aa11
TrungYU <quoctrung1220@gmail.com>
2019-03-31 22:25:28 -0700

    190331

--- js/index_slide.js
+++ js/index_slide.js
@@ -11,7 +11,7 @@ function currentDiv(n) {
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
+  var dots = document.getElementsByClassName("demodots");
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
