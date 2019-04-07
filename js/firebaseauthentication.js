f9926551e99e8bbefbb1f1180fd17927ea49a608
TrungYU <quoctrung1220@gmail.com>
2019-04-06 21:12:31 -0700

    a

--- js/firebaseauthentication.js
+++ js/firebaseauthentication.js
@@ -16,20 +16,16 @@ callbacks: {
 },
 // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
 signInFlow: 'popup',
-signInSuccessUrl: 'logged_in.html',
+signInSuccessUrl: 'index.html',
 signInOptions: [
     // Leave the lines as is for the providers you want to offer your users.
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-    firebase.auth.GithubAuthProvider.PROVIDER_ID,
-    firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    firebase.auth.PhoneAuthProvider.PROVIDER_ID
+
+    firebase.auth.EmailAuthProvider.PROVIDER_ID
 ],
 // Terms of service url.
-tosUrl: 'logged_in.html',
+tosUrl: 'index.html',
 // Privacy policy url.
-privacyPolicyUrl: 'logged_in.html'
+privacyPolicyUrl: 'index.html'
 };
 // The start method will wait until the DOM is loaded.
 ui.start('#firebaseui-auth-container', uiConfig);
