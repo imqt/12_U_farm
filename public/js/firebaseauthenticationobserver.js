276ecfbeca931336fd1d672e038d818c07fbfb1e
Dillon Yeh <dillony3h@gmail.com>
2019-04-07 15:05:00 -0700

    firebase auth

---
+++ public/js/firebaseauthenticationobserver.js
@@ -0,0 +1,16 @@
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        // User is signed in.
+        var displayName = user.displayName;
+        var email = user.email;
+        var emailVerified = user.emailVerified;
+        var photoURL = user.photoURL;
+        var isAnonymous = user.isAnonymous;
+        var uid = user.uid;
+        var providerData = user.providerData;
+        // ...
+    } else {
+        // User is signed out.
+        // ...
+    }
+});
